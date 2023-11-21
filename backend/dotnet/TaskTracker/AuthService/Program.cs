58d455d16acda85278cca51e201200e6af922512
Paul Soladoye <paul.soladoye.dev@gmail.com>
2023-11-21 01:32:26 -0500

    Checkpoint

---
+++ backend/dotnet/TaskTracker/AuthService/Program.cs
@@ -0,0 +1,43 @@
+using AuthService.Extensions;
+using Serilog;
+using Telemetry.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddCustomConfiguration();
+builder.AddCustomSerilog();
+builder.AddCustomViewsAndControllers();
+builder.AddCustomOpenIddict();
+
+var app = builder.Build();
+
+if (!app.Environment.IsEnvironment("Testing") && app.Configuration.ShouldConfigureSerilog())
+{
+    app.UseSerilogRequestLogging(opts =>
+    {
+        opts.IncludeQueryInRequestPath = true;
+    });
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+try
+{
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    app.Logger.LogCritical(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
